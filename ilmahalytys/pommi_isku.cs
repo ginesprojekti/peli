ab9adc0230283c6503f6d297fdea3fa1f1cfad60
ginesprojekti <45938003+ginesprojekti@users.noreply.github.com>
2018-12-18 18:09:02 +0200

    Add files via upload

---
+++ ilmahalytys/pommi_isku.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pommi_isku : MonoBehaviour {
+
+    public GameObject pommi = null;
+
+    private float laskuri = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.laskuri += (Time.deltaTime * 30f);
+
+        if (this.laskuri >= 30f && this.laskuri <= 31.5f)
+        {
+            GameObject apupommi = Instantiate(this.pommi, new Vector3(Random.Range(-8f, 3.5f), Random.Range(8f, 15f), 0f), Quaternion.identity);
+            
+
+
+        }
+
+        if(this.laskuri >= 150f)
+        {
+
+            this.laskuri = 0;
+
+        }
+
+
+
+       
+
+	}
+}
---
+++ ilmahalytys/pommipudotus.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pommipudotus : MonoBehaviour {
+
+
+    public GameObject rajahdys = null;
+    private float laskuri = 0f;
+    
+
+
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.laskuri += Time.deltaTime;
+
+
+
+
+        if (this.GetComponent<Transform>().position.y < -4f)
+        {
+
+            GameObject apuRajahdys = Instantiate(this.rajahdys, this.GetComponent<Transform>().position, Quaternion.identity);
+            Destroy(apuRajahdys.gameObject, 1f);
+            Destroy(this.gameObject);
+            
+
+        }
+
+        if (this.laskuri > 10f)
+        {
+
+        }
+		
+	}
+
+
+    /*void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name.Equals("taustatesti"))
+        {
+
+            GameObject apuRajahdys = Instantiate(this.rajahdys, this.GetComponent<Transform>().position, Quaternion.identity);
+            Destroy(apuRajahdys.gameObject, 2f);
+            Destroy(this.gameObject);
+
+        }
+    } */
+
+
+}
