e1ea1124b94feb32bffd12c3ffffefd4b2acabf3
ginesprojekti <45938003+ginesprojekti@users.noreply.github.com>
2018-12-19 10:42:42 +0200

    Add files via upload

---
+++ aloitusjapause/kummatkin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class kummatkin : MonoBehaviour
+{
+    public GameObject logo;
+    
+    public float aika = 0;
+    
+    private void Start()
+    {
+        logo = GameObject.Find("nimi");
+    }
+    public void Pelaa()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void Lopeta()
+    {
+
+        Application.Quit();
+    }
+    private void Update()
+    {
+        aika = aika + 0.04f;
+        if(aika > 0 && aika <10)
+        {
+            logo.transform.position = new Vector3(logo.transform.position.x, logo.transform.position.y + 0.05f, 0);
+        }
+        if (aika >= 10 && aika <=20)
+        {
+            logo.transform.position = new Vector3(logo.transform.position.x, logo.transform.position.y - 0.05f, 0);
+            
+        }
+        if(aika > 20)
+        {
+
+            aika = 0;
+        }
+    }
+
+
+}
---
+++ aloitusjapause/pause.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class pause : MonoBehaviour {
+
+    public static bool Pausella = false;
+    public GameObject pauseMenuUI;
+    public aika aika;
+    // Update is called once per frame
+    void Start()
+    {
+        aika = GameObject.FindGameObjectWithTag("aika2").GetComponent<aika>();
+    }
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Pausella)
+            {
+                Resume();
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                Pause(); 
+
+            }
+        }
+	}
+   public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Pausella = false;
+
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Pausella = true;
+    }
+    public void Menuun()
+    {
+
+        Time.timeScale = 1f;
+        Application.Quit();
+
+    }
+    public void Alkuun()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("menu");
+        aika.aika2 = 0;
+
+        Pausella = false;
+
+    }
+}
