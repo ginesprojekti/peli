5a87610c037ec3e3ffdf6cfd5a522bed5bbd5def
ginesprojekti <45938003+ginesprojekti@users.noreply.github.com>
2018-12-19 16:16:16 +0200

    Add files via upload

---
+++ lumiukkokoodi1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lumiukkokoodi1 : MonoBehaviour {
+
+
+    public float speed = 0.005f;
+    Vector3 pointA;
+    Vector3 pointB;
+
+    void Start()
+    {
+        pointA = new Vector3(-19, 4, 0);
+        pointB = new Vector3(55, 4, 0);
+    }
+
+    void Update()
+    {
+        
+        float time = Time.time * speed;
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+    }
+}
